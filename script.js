3335eea35ef250e2d0cecc3ad87e01bf57b73cca
Paho Josue <pahotchaptchet4@gmail.com>
2024-08-10 01:19:30 +0100

    Big modifications

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function UpdateTheSecondDropdown()
     secondDropdown.innerHTML = "";
 
     var options = {
-        RIA: ["V6 CEMAC", "V8 UEMOA","GUINEE_EQUATORIALE", "TX BENIN", "TW US ET CANADA", "TY FRANCE ET BELGIQUE", "VZ INTERNATIONAL"],
+        RIA: ["V6 CEMAC", "V8 UEMOA","GUINEE EQUATORIALE", "TX BENIN", "TW US ET CANADA", "TY FRANCE ET BELGIQUE", "VZ INTERNATIONAL"],
         MONEYGRAM: [],
-        WESTERN_UNION: []
+        WESTERN_UNION: ["h"]
     };
 
     if(options[selectedValue])
@@ -23,6 +23,24 @@ function UpdateTheSecondDropdown()
         secondDropdown.appendChild(option);
     });
     }
+    if(selectedValue == "WESTERN_UNION")
+    {
+        var elements = document.getElementsByClassName("nomMoneygram");
+        for(var i = 0; i < elements.length; i++)
+            {
+                elements[i].textContent = "WU";
+            }
+        document.getElementById("MontantAchanger").innerHTML = "5,000,000";
+    }
+    if(selectedValue != "WESTERN_UNION")
+    {
+        var elements = document.getElementsByClassName("nomMoneygram");
+        for(var i = 0; i < elements.length; i++)
+        {
+            elements[i].textContent = "MONEYGRAM";
+        }
+        document.getElementById("MontantAchanger").innerHTML = "1,000,000"
+    }
 }
 function ReadFormData()
 {
@@ -34,38 +52,53 @@ function ReadFormData()
     formData["Montant"] = parseInt(document.getElementById("Montant").value);
     return formData;
 }
-function GenerateResults()
+async function GetHT(amount)
+{
+    return fetch('./data.json')
+            .then(res => res.json())
+            .then(data => {
+                for(let key in data.RIA.V6CEMAC)
+                    {
+                        var pos = parseInt(key.indexOf("A"));
+                        if(amount <= key.slice(pos+1, key.length))
+                        {
+                            var HT = parseInt(data.RIA.V6CEMAC[key]);                            
+                            return HT;
+                        }
+                    }
+                    return HT;
+            })
+}
+async function GenerateResults()
 {
     var formData = ReadFormData();
     if(formData.Operateur == "RIA")
     {
         if(formData.Zone == "V6 CEMAC")
-        {
-            if(formData.Montant >=0 && formData.Montant <= 10_000)
-            {
-                var HT = parseInt(939);
-                var CD = parseInt(Math.round(formData.Montant * 0.0025));
-                var TVA = parseInt(Math.round((CD + HT) * 0.1925));
-                var TTA = parseInt(Math.round(formData.Montant * 0.002));
-                var QPBACM = parseInt(Math.round(HT * 0.3));
-                var AccompteQPM = parseInt(Math.round(HT * 0.7 * 0.022));
-                var QPM = parseInt(Math.round((HT * 0.7) - AccompteQPM));
-                var TotalQPM = parseInt(Math.round(QPM + AccompteQPM));
+        {    
+            var HT = await GetHT(formData.Montant);
+            console.log(HT);
+            var CD = parseInt(Math.round(formData.Montant * 0.0025));
+            var TVA = parseInt(Math.round((CD + HT) * 0.1925));
+            var TTA = parseInt(Math.round(formData.Montant * 0.002));
+            var QPBACM = parseInt(Math.round(HT * 0.3));
+            var AccompteQPM = parseInt(Math.round(HT * 0.7 * 0.022));
+            var QPM = parseInt(Math.round((HT * 0.7) - AccompteQPM));
+            var TotalQPM = parseInt(Math.round(QPM + AccompteQPM));
 
-                var TotalTTC = formData.Montant + HT + CD + TVA + TTA;
+            var TotalTTC = formData.Montant + HT + CD + TVA + TTA;
 
-                document.getElementById("HT-value").innerText = HT;
-                document.getElementById("CD-value").innerText = CD;
-                document.getElementById("TVA-value").innerText = TVA;
-                document.getElementById("TTA-value").innerText = TTA;
-                document.getElementById("QPBACM-value").innerText = QPBACM;
-                document.getElementById("AccompteQPM-value").innerText = AccompteQPM;
-                document.getElementById("QPM-value").innerText = QPM;
-                document.getElementById("TotalQPM-value").innerText = TotalQPM;
+            document.getElementById("HT-value").innerText = HT;
+            document.getElementById("CD-value").innerText = CD;
+            document.getElementById("TVA-value").innerText = TVA;
+            document.getElementById("TTA-value").innerText = TTA;
+            document.getElementById("QPBACM-value").innerText = QPBACM;
+            document.getElementById("AccompteQPM-value").innerText = AccompteQPM;
+            document.getElementById("QPM-value").innerText = QPM;
+            document.getElementById("TotalQPM-value").innerText = TotalQPM;
 
-                document.getElementById("result").append(TotalTTC);
+            document.getElementById("result").innerText = TotalTTC;
             }
         }
     }
-}
-
+    
--- test.js
+++ test.js
@@ -1,20 +1,14 @@
 const fs = require('fs')
 
 fs.readFile("./data.json", "utf-8", (err, jsonString) =>{
-    if(err){
-        console.log(err);
-    }
-    else{
         const data = JSON.parse(jsonString);
         for(let key in data.RIA.V6CEMAC)
         {
             var pos = parseInt(key.indexOf("A"));
-            var amount = parseInt(12000);
             if(amount <= key.slice(pos+1, key.length))
             {
-                console.log(data.RIA.V6CEMAC[key]);
+                HT = data.RIA.V6CEMAC[key];
                 break;
             }
         }
-    }
 })
